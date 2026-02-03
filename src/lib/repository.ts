b414dfe47acb1483569f0095cad9e09c78281f22
Yhanis Boullonnois <yhanis@app-stack.tech>
2026-02-03 18:25:41 +0100

    feat: allow dynamic selection of conversation repository based on environment variable

--- src/lib/repository.ts
+++ src/lib/repository.ts
@@ -1,4 +1,8 @@
 import { prismaConversationRepository } from './prisma-conversation-repository';
 import type { ConversationRepository } from './conversation-repository';
+import { memoryConversationRepository } from './memory-conversation-repository';
 
-export const conversationRepository: ConversationRepository = prismaConversationRepository;
+export const conversationRepository: ConversationRepository =
+  process.env.REPOSITORY === 'MEMORY'
+    ? memoryConversationRepository
+    : prismaConversationRepository;
